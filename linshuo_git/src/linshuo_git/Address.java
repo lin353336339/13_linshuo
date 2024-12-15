1058a232a3fbd93d33b3ac7849c6bc5bac4303ec
lin353336339 <353336339@qq.com>
2024-12-15 14:59:29 +0800

    Add files via upload

---
+++ linshuo_git/src/linshuo_git/Address.java
@@ -0,0 +1,7 @@
+package linshuo_git;
+
+public class Address {
+	private String postCode;
+	private String street;
+	private String linshuo_13;
+}
---
+++ linshuo_git/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module linshuo_git {
+}
